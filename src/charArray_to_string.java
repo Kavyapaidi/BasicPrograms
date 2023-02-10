1894bf257e792c3b2055306a73c4b76ecf9142dc
Paidi Kavyasri <86728151+Kavyapaidi@users.noreply.github.com>
2023-02-10 19:39:27 +0530

    Delete src directory

--- src/charArray_to_string.java
+++
@@ -1,7 +0,0 @@
-public class charArray_to_string {
-    public static void main(String[] args) {
-        char[] ch={'k','a','v','y','a'};
-        String str=new String(ch);
-        System.out.println(str);
-    }
-}
