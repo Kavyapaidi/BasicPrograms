2a848a92e13d73278e60eab50b816770eb3a2b99
Paidi Kavyasri <86728151+Kavyapaidi@users.noreply.github.com>
2023-02-10 19:40:17 +0530

    Add files via upload

---
+++ BasicStringPrograms/charArray_to_string.java
@@ -0,0 +1,7 @@
+public class charArray_to_string {
+    public static void main(String[] args) {
+        char[] ch={'k','a','v','y','a'};
+        String str=new String(ch);
+        System.out.println(str);
+    }
+}
